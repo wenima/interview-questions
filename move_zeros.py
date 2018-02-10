05085dded248fb6d3f6c74d154819dc4a2e6a5c5
MarcMBPDev <marc.wenicker@gmail.com>
2018-02-10 13:59:51 -0800

    initial solution

--- move_zeros.py
+++ move_zeros.py
@@ -1,1 +1,7 @@
 """Solution for https://www.codewars.com/kata/kata/moving-zeros-to-the-end/"""
+
+def move_zeros(l):
+    """Return a list with all zeroes moved to the end while keeping original order
+    for all non-zero values."""
+    out = [n for n in l if n != 0]
+    return out + (len(l) - len(out)) * [0]
