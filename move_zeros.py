4bd2c55e3ed2e9101d7ec3168c8e017cfa7170bb
MarcMBPDev <marc.wenicker@gmail.com>
2018-02-12 15:13:49 -0800

    fix code to deal with None and distinguish between False and the int value zero

--- move_zeros.py
+++ move_zeros.py
@@ -3,5 +3,11 @@
 def move_zeros(l):
     """Return a list with all zeroes moved to the end while keeping original order
     for all non-zero values."""
-    out = [n for n in l if n != 0]
+    out = []
+    for n in l:
+        if not any([type(n) is int, type(n) is float]):
+            out.append(n)
+            continue
+        if not int(n) is 0:
+            out.append(n)
     return out + (len(l) - len(out)) * [0]
