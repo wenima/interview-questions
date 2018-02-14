b76e7160db07db216309b3e588b740cc357f9d23
MarcMBPDev <marc.wenicker@gmail.com>
2018-02-14 14:29:34 -0800

    intial commit for https://www.codewars.com/kata/reverse-a-number/train/python

---
+++ test_reverse_number.py
@@ -0,0 +1,18 @@
+"""Solution for https://www.codewars.com/kata/reverse-a-number"""
+
+import pytest
+
+TESTS = [
+    (123, 321),
+    (-123, -321),
+    (1000, 1),
+    (4321234, 4321234),
+    (5, 5),
+    (98989898, 89898989),
+]
+
+@pytest.mark.parametrize('source, result', TESTS)
+def test_reverse_number(source, result):
+    """Test that function reverse_number returns the correct result."""
+    from reverse_number import reverse_number
+    assert reverse_number(source) == result
