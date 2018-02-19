d17872fa1754744b55ab63cb3c3ca83dba3e1f47
MarcMBPDev <marc.wenicker@gmail.com>
2018-02-19 10:31:46 -0800

    add ceil to round up

--- counterfeit.py
+++ counterfeit.py
@@ -1,5 +1,7 @@
 """Tests for for https://www.codewars.com/kata/number-of-measurements-to-spot-the-counterfeit-coin/"""
 
+from math import ceil
+
 def how_many_measurements(n):
     """Return the number of measurements it would take to find the counterfeit coin within n coins."""
     if n == 1: return 0
@@ -8,5 +10,5 @@ def how_many_measurements(n):
     i = 0
     while n > 9:
         i += 1
-        n //= 3
+        n = ceil(n / 3)
     return i + 2
