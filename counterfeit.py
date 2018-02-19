2b8dda6ce366af75f26396d2215c18d72f1b4971
MarcMBPDev <marc.wenicker@gmail.com>
2018-02-18 16:11:38 -0800

    add function to return number of tries to find counterfeit coin

--- counterfeit.py
+++ counterfeit.py
@@ -2,4 +2,11 @@
 
 def how_many_measurements(n):
     """Return the number of measurements it would take to find the counterfeit coin within n coins."""
-    return min_measurements
+    if n == 1: return 0
+    if n in [2, 3]: return 1
+    if n < 10: return 2
+    i = 0
+    while n > 9:
+        i += 1
+        n //= 3
+    return i + 2
